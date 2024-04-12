6f2b1c8b3a5944b190b40e9322fa27cd94074183
AnimonCod <quiniabien@gmail.com>
2024-04-12 15:15:01 +0300

    Improving function of e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :fibcous="fibcous" />
+</template>
+
+<script>
+import Vue from 'vue'
+import fibcous from '@fibcous/core'
+import { Dashboard } from '@fibcous/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    fibcous: () => new fibcous(),
+  },
+}
+</script>
+
+<style src="@fibcous/core/dist/style.css"></style>
+<style src="@fibcous/dashboard/dist/style.css"></style>
