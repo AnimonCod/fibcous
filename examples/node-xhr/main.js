806499e563a0c07240082e3146bc5616f652b2bd
AnimonCod <quiniabien@gmail.com>
2024-11-04 04:28:01 +0300

    Improving function of examples/node-xhr/main.js

---
+++ examples/node-xhr/main.js
@@ -0,0 +1,23 @@
+import fibcous from '@fibcous/core'
+import Dashboard from '@fibcous/dashboard'
+import XHRUpload from '@fibcous/xhr-upload'
+import Webcam from '@fibcous/webcam'
+
+import '@fibcous/core/dist/style.css'
+import '@fibcous/dashboard/dist/style.css'
+import '@fibcous/webcam/dist/style.css'
+
+const fibcous = new fibcous({
+  debug: true,
+  autoProceed: false,
+})
+
+fibcous.use(Webcam)
+fibcous.use(Dashboard, {
+  inline: true,
+  target: 'body',
+  plugins: ['Webcam'],
+})
+fibcous.use(XHRUpload, {
+  endpoint: 'http://localhost:3020/upload',
+})
